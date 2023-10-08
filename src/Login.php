4d7fa0551dffb8e718bb6faf8416d96063e6f0b8
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2023-10-08 11:43:19 -0400

    New dialog to fix blurring

--- src/Login.php
+++ src/Login.php
@@ -8,13 +8,13 @@ $conn = (new DatabaseConnector())->getConnection();
 // Check if the necessary POST data keys ('user', 'password') are set.
 if (!isset($_POST['Login'], $_POST['Password'])) {
     // If any of these keys are not set, exit and output an error message.
-    echo '<script>createAlert("Please fill both the username and password fields!")</script>';
+    echo '<script>createAlertDialog("Please fill both the username and password fields!", "danger")</script>';
     exit;
 }
 
 if (empty($_POST['Login']) || empty($_POST['Password'])) {
     // there is an empty field
-    echo '<script>createAlert("Please fill both the username and password fields!")</script>';
+    echo '<script>createAlertDialog("Please fill both the username and password fields!", "danger")</script>';
     exit;
 }
 
@@ -40,12 +40,12 @@ else {
             $_SESSION['name'] = $_POST['Login'];
             $_SESSION['id'] = $id;
 
-            echo '<script>createAlert("You have successfully logged in!", "danger")</script>';
+            echo '<script>createAlertDialog("You have successfully logged in!", "danger")</script>';
 
             // Redirect to the Login dashboard or home page.
             header('Location: Home.php');
             exit;
-    }
+        }
         else {
             // Password is incorrect, display an error message.
             echo $_POST['Password'];
@@ -55,8 +55,8 @@ else {
     }
     else {
         // Loginname does not exist
-        echo '<script>createAlert("No Loginname exists with that login!", "danger")</script>';
+        echo '<script>createAlertDialog("No Loginname exists with that login!", "danger")</script>';
     }
 }
-    $stmt = null;
+$stmt = null;
 ?>
