9dd521e81589ed3189de6c79c52151bd43f9e347
Alejandro Palmar <alejandrojpalmar@hotmail.com>
2023-10-09 00:04:19 -0400

    Final commit. Signup, login, search and editting

--- src/js/APIcalls.js
+++ src/js/APIcalls.js
@@ -33,6 +33,11 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+function editContact(contact) {
+    sessionStorage.setItem('editContactData', JSON.stringify(contact));
+    window.location.href = "./EditingPage.php";
+}
+
 function getContacts() {
 
     const toQuery = document.getElementById("searchData").value;    
@@ -43,7 +48,7 @@ function getContacts() {
     });
     console.log(toQuery);
     
-    fetch("http://localhost/src/SearchContact.php?" + params)
+    fetch("http://138.197.100.219/LAMPAPI/src/SearchContact.php?" + params)
     .then(data => {
         if (data.ok) {
             return data.json();
@@ -71,13 +76,42 @@ function populateResults(data) {
 
         card.className = "itemsCard";
         card.dataset.key = item.id;
-        
+
         card.innerHTML = `
             <div>${item.firstname}</div>
             <div>${item.lastname}</div>
             <div>${item.email}</div>
             <div>${item.phone}</div>
         `;
+
+        const editButton = document.createElement("button");
+        editButton.type = "button";
+        editButton.className = "continueBtn col-span-4 p-2";
+        editButton.textContent = "Edit";
+
+        editButton.onclick = function() {
+            editContact(item);
+        };
+        
+        card.appendChild(editButton);
         container.appendChild(card);
-    })
+    });
+
 }
+
+// Get the data from the previous page
+
+document.addEventListener('DOMContentLoaded', function() {
+    const contactData = JSON.parse(sessionStorage.getItem('editContactData'));
+    if (contactData) {
+        // Populate the edit form fields with the contactData values
+        document.getElementById("firstname").value = contactData.firstname;
+        document.getElementById("lastname").value = contactData.lastname;
+        document.getElementById("email").value = contactData.email;
+        document.getElementById("phone").value = contactData.phone;
+        document.getElementById("contactId").value = contactData.id;
+        
+    } else {
+        console.error("No contact data found for editing.");
+    }
+});
